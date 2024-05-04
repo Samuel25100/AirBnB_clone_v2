42d8779d70de22c4d97b0c1963e099d3cda3c47f
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-04 09:27:15 +0300

    add file 0-

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -18,15 +18,12 @@ echo "$content" > /data/web_static/releases/test/index.html
 
 sudo mkdir /data/web_static/current
 
-if [ -L "/data/web_static/releases/test/" ]
-then
-	rm -f /data/web_static/releases/test/
-fi
-sudo ln -s /data/web_static/current /data/web_static/releases/test/
-
-sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default_bak
+sudo ln -s -f /data/web_static/current /data/web_static/releases/test/
 
 sudo chown -R ubuntu:ubuntu /data/
 
+sudo ufw allow 'Nginx HTTP'
+
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
