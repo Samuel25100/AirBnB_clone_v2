b56011051ed351d4902fad00692d6e620e9d4ce5
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-17 07:53:20 +0300

    add file 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Start a Flask web application and return simple string."""
+import web_flask
+
+app = web_flask.Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnh():
+    return "HBNB"
+
+
+@app.route("/c/<word>", strict_slashes=False)
+def C(word):
+    word = word.replace('_', ' ')
+    return f"C {word}"
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<word>", strict_slashes=False)
+def python(word='is cool'):
+    word = word.replace('_', ' ')
+    return f"Python {word}"
+
+
+@app.route("/number/<int:num>", strict_slashes=False)
+def number(num):
+    return f"{num} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
