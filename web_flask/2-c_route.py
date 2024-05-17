b7a7a829bf30f2ffa1795b21c07d0eecb87b2a9b
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-17 07:40:43 +0300

    add file 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start a Flask web application and return simple string."""
+import web_flask
+
+app = web_flask.Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnh():
+    return "HBNB"
+
+
+@app.route("/c/<word>", strict_slashes=False)
+def C(word):
+    word = word.replace('_', ' ')
+    return f"C {word}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
