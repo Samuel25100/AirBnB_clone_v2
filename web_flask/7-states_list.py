db2a1a5842ef1c3077ebe5f262d287d95338afec
Samuel25100 <samuelgselassie1@gmail.com>
2024-05-19 11:15:48 +0300

    add file 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Start a Flask web application and return simple string."""
+from models import storage
+from models import State
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def State_list():
+    dic = storage.all(State)
+    result = {}
+    for val in dic.values():
+        result[val.name] = val.id
+    sorted_dict = dict(sorted(result.items()))
+    return render_template('7-states_list.html', dic=sorted_dict)
+
+
+@app.teardown_appcontext
+def clean(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
